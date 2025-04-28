bc45de1b77d44c51b97a5fe656b7c1101ae618f5
ntua-el20160 <147728876+ntua-el20160@users.noreply.github.com>
2025-04-29 02:04:34 +0300

    a

--- engine.py
+++ engine.py
@@ -125,6 +125,8 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
        
         # optimizer.zero_grad()
         loss.backward()
+        if max_norm > 0:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm)
         optimizer.step()
         torch.cuda.synchronize()
 
