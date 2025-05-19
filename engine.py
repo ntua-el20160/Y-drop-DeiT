cfa710adb932aed6cb64e441e225179f4b74383b
ntua-el20160 <147728876+ntua-el20160@users.noreply.github.com>
2025-05-19 17:00:16 +0300

    Update engine.py

--- engine.py
+++ engine.py
@@ -182,8 +182,8 @@ def evaluate(data_loader, model, device):
 
     # switch to evaluation mode
     model.eval()
-
-    for images, target in metric_logger.log_every(data_loader, 10, header):
+    print_freq = 30
+    for images, target in metric_logger.log_every(data_loader, print_freq, header):
         images = images.to(device, non_blocking=True)
         target = target.to(device, non_blocking=True)
 
@@ -198,6 +198,7 @@ def evaluate(data_loader, model, device):
         metric_logger.update(loss=loss.item())
         metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
         metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
+        
 
     print('* Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f} loss {losses.global_avg:.3f}'
           .format(top1=metric_logger.acc1, top5=metric_logger.acc5, losses=metric_logger.loss))
