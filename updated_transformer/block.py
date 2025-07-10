9c59695f73fbe2c53d0f90f693bd8994559fcb92
ntua-el20160 <el20160@mail.ntua.gr>
2025-07-10 14:50:30 +0300

    fix

--- updated_transformer/block.py
+++ updated_transformer/block.py
@@ -44,7 +44,8 @@ class Block(nn.Module):
             scaler: Optional[float] = 1.0,
             transformer_mean: bool = False,
             rescaling_type: Optional[str] = None,
-            scale_attn_norm: bool = False,
+            scale_attn_norm: bool = False,   # <— new
+            **kwargs, 
 
 
     ) -> None:
